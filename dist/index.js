d61ce3417c7d00b6a88b167b81989ac107bfbdfc
valente alvarez <valvarez@basecampcodingacademy.org>
2017-10-23 11:19:45 -0500

    move file in here.

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+
+function Makegame() {
+    this.blocks = function() {};
+    this.ball = function() {};
+    this.wall = function() {};
+    this.collision = function() {};
+    this.keyscontrol = function() {};
+    this.mousecontrol = function() {};
+    this.gameover = function() {};
+    this.score = function() {};
+}
