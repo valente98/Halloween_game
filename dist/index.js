0145ca09eb29c4db5d347d3f9948feeceee1e412
valente alvarez <valvarez@basecampcodingacademy.org>
2017-10-24 12:59:04 -0500

    change the order of the letters in the grid. created a function that will shulfle the word of the array.

--- dist/index.js
+++ dist/index.js
@@ -5,15 +5,33 @@
 function turnblue(td) {
     $(td).addClass('blue');
 }
-function swap(td) {
+function addLetters() {
     var grid = [
         [null, 'H', 'A', 'P'],
-        s[('P', 'Y', 'H', 'A')],
+        ['P', 'Y', 'H', 'A'],
         ['L', 'L', 'O', 'W'],
         ['E', 'E', 'N', '!']
     ];
-    var empty = [0, 0];
-
-    for (letter in grid) {
-    }
+    return grid;
 }
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+function displayLetters() {
+    var arr = shuffle(addLetters());
+    $('#letter').append(arr);
+}
+
+function swap(td) {}
